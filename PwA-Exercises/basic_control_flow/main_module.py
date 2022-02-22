e3466f70f9e9e0ca6990223e3706070bcb25e90b
eSuminski <rctolinski@gmail.com>
2022-02-22 16:55:50 -0500

    added the first group of practice excercises

---
+++ PwA-Exercises/basic_control_flow/main_module.py
@@ -0,0 +1,47 @@
+"""Control Flow And String Exercises"""
+
+"""
+Create a for loop that prints the numbers zero through ten sequentially
+"""
+
+"""
+Create a for loop that prints the numbers 20 through 10 sequentially
+"""
+
+"""
+Create a while loop that prints a string of * that grow by one each print until the string printed is 10 * together
+"""
+
+"""
+Create a string with the value "world!" in a new module and import it to this one. Once you have done that, print
+the phrase "Hello world!" using the variable you imported from the new module
+"""
+
+"""
+Print the phrase "Hello World" using an f string and the imported variable you used for the previous exercise
+"""
+
+"""
+Create a new string variable and give it the value of your name (example: my_name = "Eric"). Then, print a greeting 
+to yourself using the string format method
+"""
+
+"""
+Create a string with the following value: 'It is a lovely day today'. Write out six print statements that each print
+a single word from the string using string slicing
+"""
+
+"""
+Use that same string and print the words backwards, starting from the last word, one by one
+"""
+
+"""
+Create two number variables called num_one and num_two, and assign them any value that is a whole number between 1 
+and 100. Then create an if/else statement that prints True if num_one divided by num_two is a whole number, or False
+if the numbers divided make a fraction. If you get True back change the numbers to try and get False, and vice versa
+"""
+
+"""
+Create a loop that prints the numbers one through five a total of five times. It also needs to tell you which 
+iteration of counting the loop is currently on 
+"""
---
+++ PwA-Exercises/string_data_structure_manipulation/string_ds_excercises.py
@@ -0,0 +1,28 @@
+"""
+Below are four functions, each meant to convert a string into a specific naming convention. Write the logic for each
+function that will allow them to return the given string in the function's given naming convention
+"""
+
+phrase = "This is my test phrase."
+
+
+def make_snake_case(words: str):
+    pass
+
+
+def make_camelCase(words: str):
+    pass
+
+
+def make_SCREAMING_SNAKE_CASE(words: str):
+    pass
+
+
+def make_PascalCase(words: str):
+    pass
+
+
+print(make_snake_case(phrase))
+print(make_camelCase(phrase))
+print(make_SCREAMING_SNAKE_CASE(phrase))
+print(make_PascalCase(phrase))
