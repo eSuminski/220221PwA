8a3e0470f2e34cc6c79dbcc5316cf81f8129b216
eSuminski <rctolinski@gmail.com>
2022-04-13 16:15:10 -0400

    adding changes to sql script

--- Week8/Day3/ConnectingToPostgres/Script-to-set-up-database-and-test-data.sql
+++ Week8/Day3/ConnectingToPostgres/Script-to-set-up-database-and-test-data.sql
@@ -8,7 +8,7 @@ create table players(
 insert into players values(-1,'Billy','Bob');
 -- needed for update player test, need to update after tests
 insert into players values(-2,'needs','names');
-update players set first_name = 'needs' where player_id = -2;
+update players set first_name = 'needs', last_name ='names' where player_id = -2;
 
 -- needed for delete player test
 insert into players values(-3,'delete','me');
